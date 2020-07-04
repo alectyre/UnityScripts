3be41c6d64eed6d28669f8408f07a42259f927f9
Alec <alectyre@gmail.com>
2020-07-04 09:34:43 -0700

    Added ReplaceWithPrefab, SimpleTrigger, and StepRotate.

---
+++ SimpleTrigger/SimpleTrigger.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class SimpleTrigger : MonoBehaviour {
+    [SerializeField] LayerMask layermask;
+    [Space]
+    [SerializeField] UnityEvent OnEnter;
+    [SerializeField] UnityEvent OnExit;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (OnEnter != null &&
+            layermask == (layermask | (1 << collision.gameObject.layer)))
+        {
+            OnEnter.Invoke();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (OnEnter != null &&
+            layermask == (layermask | (1 << other.gameObject.layer)))
+        {
+            Debug.Log("trigger enter " + gameObject.name);
+
+            OnEnter.Invoke();
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (OnExit != null &&
+            layermask == (layermask | (1 << collision.gameObject.layer)))
+        {
+            OnExit.Invoke();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (OnExit != null &&
+            layermask == (layermask | (1 << other.gameObject.layer)))
+        {
+            Debug.Log("trigger exit " + gameObject.name);
+
+            OnExit.Invoke();
+        }
+    }
+}
---
+++ StepRotate/StepRotate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StepRotate : MonoBehaviour {
+
+    [Tooltip("Degrees per second")] public float speed = 90;
+    [Tooltip("Step size in degrees")] public float stepSize = 45;
+    public Vector3 axis = Vector3.forward;
+	
+    float toRotate;
+
+	void Update () 
+	{
+        toRotate += Time.deltaTime * Mathf.Abs(speed);
+
+        if(stepSize <= 0)
+        {
+            transform.Rotate(axis, Time.deltaTime * speed);
+        }
+        else if(speed != 0 && toRotate >= stepSize)
+        {
+            transform.Rotate(axis, speed > 0 ? stepSize : -stepSize);
+
+            toRotate -= stepSize;
+        }
+	}
+}
